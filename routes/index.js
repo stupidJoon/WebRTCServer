3c63687f9853a0d49165f22a2fff975785057b43
stupidJoon <junsang.yu3@gmail.com>
2019-09-15 15:14:43 +0900

    a

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/caller', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/Caller/index.html'))
+});
+router.get('/callee', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/Callee/index.html'))
 });
 
 module.exports = router;
