f55e2ac2ef742e92a732794047032bde22f71d25
stupidJoon <junsang.yu3@gmail.com>
2019-09-16 22:05:08 +0900

    a

--- socket.js
+++ socket.js
@@ -2,6 +2,8 @@ var exports = module.exports = {};
 
 function webRTC(io) {
   io.on('connection', (socket) => {
+    console.log('Socket Connected', socket.id);
+
     socket.on('join', (room) => {
       if (room == "caller") {
         socket.join('caller');
@@ -14,21 +16,24 @@ function webRTC(io) {
       }
       console.log(socket.id, 'is', room);
     });
-    console.log('Socket Connected', socket.id);
+
     socket.on('offer', (offer) => {
       console.log('Offer', offer != null);
       io.sockets.emit('offer', offer);
     });
+
     socket.on('answer', (answer) => {
       console.log('Answer', answer != null);
       io.sockets.emit('answer', answer);
     });
+
     socket.on('candidate', (candidate) => {
       console.log('Candidate', candidate != null);
       io.sockets.emit('candidate', candidate);
     });
+
     socket.on('disconnect', () => {
-      console.log('Socket Disconnected', socket.id);
+      console.log(socket.room, 'Socket Disconnected', socket.id);
     });
   });
 }
