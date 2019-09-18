c3c7b7df636ec82d47327bf40b2a516976f37ead
stupidJoon <junsang.yu3@gmail.com>
2019-09-18 21:42:47 +0900

    a

--- public/javascripts/callee.js
+++ public/javascripts/callee.js
@@ -1,15 +1,10 @@
 const RTC_CONFIGURATION = {
-  iceServer: [
+  iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
     {
-      urls: 'turn:192.158.29.39:3478?transport=udp',
-      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      username: '28224511:1379330808'
-    },
-    {
-      urls: 'turn:192.158.29.39:3478?transport=tcp',
-      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      username: '28224511:1379330808'
+      url: 'turn:numb.viagenie.ca',
+      credential: 'muazkh',
+      username: 'webrtc@live.com'
     }
   ]
 };
